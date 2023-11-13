8304263f854d6ff401aa992334bb0f8b9225e7b1
kubagurPL <51764134+kubagurPL@users.noreply.github.com>
2023-11-12 20:58:40 -0500

    Update processHTMLreq.php

--- processHTMLreq.php
+++ processHTMLreq.php
@@ -2,7 +2,19 @@
 
   <body>
     <h1>Thank you for submitting your HTML!</h1>
-    You submitted:<?php echo $_POST["ATML"]; ?>
+    You submitted:<?php echo $_POST["ATML"]; 
+    $ATML = $_POST["ATML"];
+    $mainFile = fopen("ATMLpage.php","a") or die("Unable to open file!");
+    fwrite($mainFile,$ATML);
+    fwrite($maniFile,"\n");
+    ?>
+    
+    <button onclick=goHome>Go back!</button>
+    <script>
+      function goHome(){
+        window.location.href = "index.php";
+      }
+    </script>
   </body>
   
 </html>
